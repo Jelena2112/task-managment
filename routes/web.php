6afbc29a5d043a6fec62b93b307b2afcd7e371c9
ITMentorstva <117232761+ITMentorstva@users.noreply.github.com>
2023-12-06 21:30:07 +0100

    * Laravel auth added

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'pages.home')->name('homepage');
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
